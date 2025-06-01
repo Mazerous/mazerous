c730bf8289e07bc653ef69901e9c52eb6050ee4a
Mazerous <amolketin@gmail.com>
2025-06-01 14:56:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const enterButton = document.getElementById("enter-button");
+  const enterPanel = document.getElementById("enter-panel");
 
+  enterButton.addEventListener("click", () => {
+    enterPanel.style.opacity = "0";
+    enterPanel.style.transition = "opacity 0.6s ease";
+    setTimeout(() => {
+      enterPanel.style.display = "none";
+    }, 600);
+  });
+});
